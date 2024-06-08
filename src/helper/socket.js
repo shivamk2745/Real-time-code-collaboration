b469d6336c9a81607388e1f1271cfaa6e1f6f02d
Shivamk2745 <shivamkashyap0126@gmail.com>
2024-06-08 14:24:04 +0530

    attemp increase

--- src/helper/socket.js
+++ src/helper/socket.js
@@ -5,7 +5,7 @@ export const initSocket = async () => {
   const option = {
     forceNew: true,
     path: "/socket",
-    reconnectionAttempts: 1000,
+    reconnectionAttempts: 10000,
     timeout: 10000,
     transports: ["websocket", "polling"],
   };
